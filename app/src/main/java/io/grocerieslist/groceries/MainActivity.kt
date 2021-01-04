284583216ca67303e52364b903a06a2414d4dbfd
Stephan Branczyk <stephan@branczyk.org>
2021-01-04 01:09:02 -0800

    create database schema, model, repo, and model view

--- app/src/main/java/io/grocerieslist/groceries/MainActivity.kt
+++ app/src/main/java/io/grocerieslist/groceries/MainActivity.kt
@@ -2,10 +2,14 @@ package io.grocerieslist.groceries
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupActionBarWithNavController(findNavController(R.id.fragment))
     }
 }
